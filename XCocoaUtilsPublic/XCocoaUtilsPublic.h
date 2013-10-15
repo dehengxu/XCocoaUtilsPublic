98482e0fa32b4de4363864ecd1b25e5db09b78b2
DehengXu <dehengxu@outlook.com>
2013-10-15 10:27:07 +0800

    Initial Commit

---
+++ XCocoaUtilsPublic/XCocoaUtilsPublic.h
@@ -0,0 +1,13 @@
+//
+//  XCocoaUtilsPublic.h
+//  XCocoaUtilsPublic
+//
+//  Created by Deheng.Xu on 13-10-15.
+//  Copyright (c) 2013年 Deheng.Xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XCocoaUtilsPublic : NSObject
+
+@end
---
+++ XCocoaUtilsPublic/XCocoaUtilsPublic.m
@@ -0,0 +1,13 @@
+//
+//  XCocoaUtilsPublic.m
+//  XCocoaUtilsPublic
+//
+//  Created by Deheng.Xu on 13-10-15.
+//  Copyright (c) 2013年 Deheng.Xu. All rights reserved.
+//
+
+#import "XCocoaUtilsPublic.h"
+
+@implementation XCocoaUtilsPublic
+
+@end
