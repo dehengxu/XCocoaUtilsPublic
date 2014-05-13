7ed8d1833df22cdfca7687fdb1e0b54e317afde7
Deheng <dehengxu@outlook.com>
2014-05-13 19:43:35 +0800

    update

--- src/XCocoaUtilsPublic/FunctionSet.h
+++ src/XCocoaUtilsPublic/FunctionSet.h
@@ -195,6 +195,63 @@
 #define UIViewAutoresizeFlexibleAround UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleRightMargin
 #endif
 
+#ifndef SYNTHESIZE_SINGLETON_FOR_CLASS
+#define SYNTHESIZE_SINGLETON_FOR_CLASS(classname) \
+\
+static classname *shared##classname = nil; \
+\
++ (classname *)shared##classname \
+{ \
+@synchronized(self) \
+{ \
+if (shared##classname == nil) \
+{ \
+shared##classname = [[self alloc] init]; \
+} \
+} \
+\
+return shared##classname; \
+} \
+\
++ (id)allocWithZone:(NSZone *)zone \
+{ \
+@synchronized(self) \
+{ \
+if (shared##classname == nil) \
+{ \
+shared##classname = [super allocWithZone:zone]; \
+return shared##classname; \
+} \
+} \
+\
+return nil; \
+} \
+\
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return self; \
+} \
+\
+- (id)retain \
+{ \
+return self; \
+} \
+\
+- (NSUInteger)retainCount \
+{ \
+return NSUIntegerMax; \
+} \
+\
+- (oneway void)release \
+{ \
+} \
+\
+- (id)autorelease \
+{ \
+return self; \
+}
+#endif
+
 #pragma mark - function
 
 //Used for delegate selector call, auto check delegate value and selector implementation.
