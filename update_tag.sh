2c68676238c27b49fd8be385f3932c3f42dc905b
DehengXu <dehengxu@outlook.com>
2014-09-09 19:38:46 +0800

    Update commit

--- update_tag.sh
+++ update_tag.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 tagname=$1
 
+if [ -z $tagname ]
+then
+  echo "请输入 \"tag name\" "
+  exit 0
+fi
+
+echo "Tag $tagname"
+
 git add --all
 git commit -m "Update commit"
 git push origin master
