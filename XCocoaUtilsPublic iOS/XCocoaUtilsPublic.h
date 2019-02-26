f1852f405c1be8d39652d88c91bab1c67e45f96d
DehengXu <dehengxu@outlook.com>
2019-02-27 07:33:53 +0800

    Update: exposing headers

--- XCocoaUtilsPublic iOS/XCocoaUtilsPublic.h
+++ XCocoaUtilsPublic iOS/XCocoaUtilsPublic.h
@@ -8,6 +8,49 @@
 
 #import <UIKit/UIKit.h>
 
+#import <XCocoaUtilsPublic/XLogging.h>
+#import <XCocoaUtilsPublic/NSInvocation+XCocoaUtils.h>
+#import <XCocoaUtilsPublic/XOperationTemplate.h>
+#import <XCocoaUtilsPublic/DebugUtility.h>
+#import <XCocoaUtilsPublic/NSURL+Test.h>
+#import <XCocoaUtilsPublic/UIColor+Ext.h>
+#import <XCocoaUtilsPublic/NSString+Ext.h>
+#import <XCocoaUtilsPublic/UIScrollView+Ext.h>
+#import <XCocoaUtilsPublic/NSURL+Ext.h>
+#import <XCocoaUtilsPublic/UIView+Ext.h>
+#import <XCocoaUtilsPublic/NSLayoutConstraint+Convenient.h>
+#import <XCocoaUtilsPublic/NSObject+Ext.h>
+#import <XCocoaUtilsPublic/NSObject+Debug.h>
+#import <XCocoaUtilsPublic/SKProduct+Ext.h>
+#import <XCocoaUtilsPublic/NSData+String.h>
+#import <XCocoaUtilsPublic/NSDate+Ext.h>
+#import <XCocoaUtilsPublic/UIAlertView+Ext.h>
+#import <XCocoaUtilsPublic/NSTimer+Ext.h>
+#import <XCocoaUtilsPublic/NSData+Base64.h>
+#import <XCocoaUtilsPublic/UIViewController+Ext.h>
+#import <XCocoaUtilsPublic/UIButton+Ext.h>
+#import <XCocoaUtilsPublic/UIImage+Ext.h>
+#import <XCocoaUtilsPublic/NSArray+Ext.h>
+#import <XCocoaUtilsPublic/UIFont+Ext.h>
+#import <XCocoaUtilsPublic/NSObject+PropertyToURLParams.h>
+#import <XCocoaUtilsPublic/UIScreen+Ext.h>
+#import <XCocoaUtilsPublic/NSDictionary+HTTP.h>
+#import <XCocoaUtilsPublic/NameValuePaire.h>
+#import <XCocoaUtilsPublic/XCObjectMapping.h>
+#import <XCocoaUtilsPublic/NSString+URI.h>
+#import <XCocoaUtilsPublic/XCObjectMappingDelegate.h>
+#import <XCocoaUtilsPublic/web_macros.h>
+#import <XCocoaUtilsPublic/singletone.h>
+#import <XCocoaUtilsPublic/memory_macros.h>
+#import <XCocoaUtilsPublic/CF_macros.h>
+#import <XCocoaUtilsPublic/UIKit_macros.h>
+#import <XCocoaUtilsPublic/FileIOHelper.h>
+#import <XCocoaUtilsPublic/DefaultSettings.h>
+#import <XCocoaUtilsPublic/BlockAlertView.h>
+#import <XCocoaUtilsPublic/CGFunctions.h>
+#import <XCocoaUtilsPublic/BenchMark.h>
+#import <XCocoaUtilsPublic/cbench_mark.h>
+
 //! Project version number for XCocoaUtilsPublic_iOS.
 FOUNDATION_EXPORT double XCocoaUtilsPublic_iOSVersionNumber;
 
