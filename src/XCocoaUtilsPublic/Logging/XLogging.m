046e86f82188bbeda6086d32011023baa670e3fd
DehengXu <dehengxu@outlook.com>
2020-01-04 17:28:19 +0800

    [update]: fix logger not work under submodule

--- src/XCocoaUtilsPublic/Logging/XLogging.m
+++ src/XCocoaUtilsPublic/Logging/XLogging.m
@@ -18,17 +18,21 @@ void DisableLogger(NSString *tag) {
     UpdateLoggerStatus(tag, "is_logging_enabled", @(false));
 }
 
-void UpdateLoggerStatus(NSString *tag, const char *key, id value) {
+void UpdateLoggerStatus(NSString *tag, const char *key, NSNumber *value) {
     if (![value isKindOfClass:[NSNumber class]]) {
         return;
     }
     Class c = NSClassFromString([NSString stringWithFormat:@"log_%@_swift", tag]);
     if (c) {
-        objc_setAssociatedObject(c, key, value, OBJC_ASSOCIATION_ASSIGN);
+        //
+        [c setLoggingEnabled:[value boolValue]];
+        //-- Under submodule will not works.
+        //objc_setAssociatedObject(c, key, value, OBJC_ASSOCIATION_ASSIGN);
     }
 
     c = NSClassFromString([NSString stringWithFormat:@"log_%@", tag]);
     if (c) {
-        objc_setAssociatedObject(c, key, value, OBJC_ASSOCIATION_ASSIGN);
+        [c setLoggingEnabled:[value boolValue]];
+        //objc_setAssociatedObject(c, key, value, OBJC_ASSOCIATION_ASSIGN);
     }
 }
