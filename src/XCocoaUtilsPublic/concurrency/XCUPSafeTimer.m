abb00876b67a6f33b5dbd84e75e30dddf4d73ac1
DehengXu <dehengxu@outlook.com>
2020-01-17 08:59:49 +0800

    [update]: safe timer

---
+++ src/XCocoaUtilsPublic/concurrency/XCUPSafeTimer.m
@@ -0,0 +1,83 @@
+//
+//  XCUPSafeTimer.m
+//  XCocoaUtilsPublic
+//
+//  Created by Deheng Xu on 2020/1/16.
+//  Copyright © 2020 Deheng.Xu. All rights reserved.
+//
+
+#import "XCUPSafeTimer.h"
+
+@interface XCUPSafeTimer ()
+
+@property (nonatomic, weak, nullable) NSTimer *timer;
+@property (nonatomic, assign) BOOL isReapt;
+@property (nonatomic, assign) NSTimeInterval interval;
+@property (nonatomic, assign) NSTimeInterval duration;
+@property (nonatomic, strong) NSDate *startTime;
+
+@end
+
+@implementation XCUPSafeTimer
+
++ (instancetype)timerWithInterval:(NSTimeInterval)interval block:(void(^)(NSTimer *))timerBlock
+{
+    XCUPSafeTimer *timer = [XCUPSafeTimer new];
+    timer.timerBlock = timerBlock;
+    timer.interval = interval;
+    return timer;
+}
+
++ (instancetype)repeatTimerWithInterval:(NSTimeInterval)interval block:(void (^)(NSTimer * _Nonnull))timerBlock
+{
+    XCUPSafeTimer *timer = [self timerWithInterval:interval block:timerBlock];
+    timer.isReapt = YES;
+    return timer;
+}
+
++ (instancetype)repeatTimerWithInterval:(NSTimeInterval)interval duration:(NSTimeInterval)duration block:(void(^)(NSTimer *))timerBlock
+{
+    XCUPSafeTimer *timer = [self repeatTimerWithInterval:interval block:timerBlock];
+    timer.interval = interval;
+    timer.duration = duration;
+    return timer;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.isReapt = NO;
+    }
+    return self;
+}
+
+- (void)startTimer
+{
+    self.timer = [NSTimer timerWithTimeInterval:self.interval target:self selector:@selector(handleTimer:) userInfo:nil repeats:self.isReapt];
+    [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+    self.startTime = [NSDate new];
+}
+
+- (void)cancelTimer
+{
+    if (!self.timer || !self.timer.isValid) {
+        return;
+    }
+    [self.timer invalidate];
+    self.timer = nil;
+    self.startTime = nil;
+}
+
+- (void)handleTimer:(NSTimer *)timer
+{
+    self.timerBlock(timer);
+    
+}
+
+- (void)dealloc
+{
+    NSLog(@"%s", __func__);
+}
+
+@end
