afdc22b0e6e73e57a849aee0d17a427f174fd921
DehengXu <dehengxu@outlook.com>
2020-06-28 10:21:49 +0800

    Update: compiler annotations

--- src/XCocoaUtilsPublic/macros/compiler_macros.h
+++ src/XCocoaUtilsPublic/macros/compiler_macros.h
@@ -9,9 +9,17 @@
 #ifndef compiler_macros_h
 #define compiler_macros_h
 
+#pragma mark - compiler attributes annotations
+
+#ifndef XCUP_ATTRIBUTE
+#define XCUP_ATTRIBUTE(x) __attribute__(x)
+#endif
+
 #ifndef XCUP_WARN_UNUSED_RESULT
 #define XCUP_WARN_UNUSED_RESULT __attribute__((warn_unused_result))
 #endif
+
+#pragma mark - compiler deprecated annotations
 
 #ifndef XCUP_DEPRECATED_MSG
 #define XCUP_DEPRECATED_MSG(x) __deprecated_msg(x)
