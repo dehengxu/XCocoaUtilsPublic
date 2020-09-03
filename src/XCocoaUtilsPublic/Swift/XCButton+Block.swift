9892e6413a8364e4861fe06efb584848884bb783
DehengXu <dehengxu@outlook.com>
2020-09-03 23:02:45 +0800

    add: block button

---
+++ src/XCocoaUtilsPublic/Swift/XCButton+Block.swift
@@ -0,0 +1,44 @@
+//
+//  NX_UIButton.swift
+//  NXPlayground
+//
+//  Created by NicholasXu on 2020/8/14.
+//  Copyright © 2020 NicholasXu. All rights reserved.
+//
+
+import UIKit
+
+@objc public extension UIButton {
+    
+    fileprivate class BlockButton: UIButton {
+        var handleBlock: ((_ sender: Any)->Void)?
+        
+        @objc func _private_onClick(_ sender: Any) {
+            if let blk = self.handleBlock {
+                blk(self)
+            }
+        }
+        
+        override func addTarget(_ target: Any?, action: Selector, for controlEvents: UIControl.Event) {
+			if (self == target as! NSObject) && self.isKind(of: UIButton.BlockButton.self) && NSStringFromSelector(action) == "_private_onClick:" {
+				super.addTarget(target, action: action, for: controlEvents)
+			}else {
+				print("BlockButton should not use selector:  \(NSStringFromSelector(#selector(UIButton.addTarget(_:action:for:))))")
+			}
+        }
+    }
+    
+    class func blockButton(type: UIButton.ButtonType , _ handler:((_ sender: Any)->Void)? = nil) -> UIButton {
+        let btn = BlockButton(type: type)
+        btn.addTarget(btn, action: #selector(BlockButton._private_onClick(_:)), for: .touchUpInside)
+        btn.handleBlock = handler
+        return btn
+    }
+    
+    @objc func setHandler(_ handler:((_ sender: Any)->Void)?) {
+        if let blkBtn = self as? BlockButton {
+            blkBtn.handleBlock = handler
+        }
+    }
+    
+}
