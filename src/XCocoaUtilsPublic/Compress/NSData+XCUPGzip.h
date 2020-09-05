adccfccfe8828facbc3de84eb4397c2a8ba8ad8e
DehengXu <dehengxu@outlook.com>
2020-09-05 17:16:38 +0800

    Merging

--- src/XCocoaUtilsPublic/Compress/NSData+XCUPGzip.h
+++ src/XCocoaUtilsPublic/Compress/NSData+XCUPGzip.h
@@ -13,8 +13,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSData (XCUPGzip)
 
-- (NSData *)xcup_gzipDataError:(NSError **)error;
-- (NSData *)xcup_ungzipDataError:(NSError **)error;
+- (NSData *)gzipDataError:(NSError **)error;
+- (NSData *)ungzipDataError:(NSError **)error;
 
 @end
 
