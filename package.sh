ce67c6cd98478beba0021fa7f6a9d1b04c3ebbc3
DehengXu <dehengxu@outlook.com>
2020-09-05 09:07:18 +0800

    update: file name

--- package.sh
+++ package.sh
@@ -3,4 +3,4 @@
 export package_path=$PWD
 # pod package --subspecs=CCommons,http,categories,macros,debug,io,benchmark,concurrency,runtime,logging,compress,$@ --exclude-deps XCocoaUtilsPublic_package.podspec
 
-pod package $@ --exclude-deps --embedded --force --no-mangle --configuration=Debug XCocoaUtilsPublic_package.podspec
+pod package $@ --exclude-deps --embedded --force --no-mangle --configuration=Debug XCocoaUtilsPublic_package
