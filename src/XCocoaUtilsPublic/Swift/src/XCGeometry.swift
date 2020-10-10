486955a85faa64e5056553d49c812b4abf0a04d1
DehengXu <dehengxu@outlook.com>
2020-10-10 10:03:58 +0800

    Add: XCGeometry

---
+++ src/XCocoaUtilsPublic/Swift/src/XCGeometry.swift
@@ -0,0 +1,14 @@
+//
+//  XCGeometry.swift
+//  XCocoaUtilsPublic
+//
+//  Created by Deheng Xu on 2020/10/10.
+//
+
+import CoreGraphics
+
+extension CGRect {
+	func xcup_center() -> CGPoint {
+		return CGPoint(x: self.origin.x + self.size.width / 2.0, y: self.origin.y + self.size.height / 2.0)
+	}
+}
